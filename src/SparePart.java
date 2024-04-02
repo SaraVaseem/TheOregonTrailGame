4191a3bc7e804589ae9f6086cbf72dcdba15c424
Connie <138333893+conniechen651@users.noreply.github.com>
2024-04-02 15:07:46 -0700

    Create classes for Items and special functions for clothing, food, and bullets

--- src/SparePart.java
+++ src/SparePart.java
@@ -1,10 +1,2 @@
-public abstract class SparePart {
-    private int count;
-
-    public void setCount(int count){
-        this.count = count;
-    }
-    public int getCount(){
-        return count;
-    }
+public abstract class SparePart extends Item{
 }
