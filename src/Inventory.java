255c2181f5de4588d63df6f724bb28ca57323e8e
Connie <138333893+conniechen651@users.noreply.github.com>
2024-04-02 15:43:34 -0700

    Create inventory class to hold all items

--- src/Inventory.java
+++ src/Inventory.java
@@ -1,3 +1,19 @@
+import java.util.ArrayList;
+
 public class Inventory {
-    
+    private ArrayList<Item> completeInventory = new ArrayList<>();
+
+    public void addItem(Item item){
+        completeInventory.add(item);
+    }
+
+    public void removeItem(Item item){
+        completeInventory.remove(item);
+    }
+
+    public void print(){
+        for(int i = 0; i < completeInventory.size(); i++){
+            System.out.println(completeInventory.get(i).getCount()+", ");
+        }
+    }
 }
