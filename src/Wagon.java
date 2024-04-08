f1879abf2b95d0545470459ee3e5917549dcbd08
Connie <138333893+conniechen651@users.noreply.github.com>
2024-04-07 18:19:03 -0700

    Add additional functions to wagon class

--- src/Wagon.java
+++ src/Wagon.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Wagon {
     private String pace;
@@ -8,6 +9,7 @@ public class Wagon {
     private ArrayList<WagonMember> listOfMembers;
     private double moneyLeft;
     private boolean wagonFunctional;
+    private int points;
 
     public Wagon(WagonMember member1, WagonMember member2, WagonMember member3, WagonMember member4, WagonMember member5){
         this.listOfMembers.add(member1);
@@ -17,53 +19,111 @@ public class Wagon {
         this.listOfMembers.add(member5);
         this.wagonFunctional = true;
         this.pace = "Steady";
+        this.rations = "Filling";
+        this.livingMembers = 5;
     }
 
     public void setPace(String pace, ArrayList<WagonMember> listOFMembers){
         this.pace = pace;
         if(pace == "Steady"){
-            spacesPerDay = 2;
+            this.spacesPerDay = 2;
             for(int i = 0; i < listOFMembers.size(); i++){
                 listOFMembers.get(i).health += 2;
             }
         } else if (pace == "Strenuous"){
-            spacesPerDay = 3;
+            this.spacesPerDay = 3;
             for(int i = 0; i < listOFMembers.size(); i++){
                 listOFMembers.get(i).health -= 1;
             }
         } else if (pace == "Grueling"){
-            spacesPerDay = 4;
+            this.spacesPerDay = 4;
             for(int i = 0; i < listOFMembers.size(); i++){
                 listOFMembers.get(i).health -= 4;
             }
         }
     }
 
-    public void move(int spaces){
+    public void setRations(String rations, Food food, ArrayList<WagonMember> listOFMembers){
+        this.rations = rations;
+        if(rations == "Generous"){
+            food.setCount(food.getCount()-3*livingMembers);
+            for(int i = 0; i < listOFMembers.size(); i++){
+                listOFMembers.get(i).health += 2;
+            }
+        } else if (rations == "Meager"){
+            food.setCount(food.getCount()-2*livingMembers);
+        } else if (rations == "Bare bones"){
+            food.setCount(food.getCount()-livingMembers);
+            for(int i = 0; i < listOFMembers.size(); i++){
+                listOFMembers.get(i).health -= 3;
+            }
+        }
+    }
 
+    public void move(int spaces, Trail trail){
+        trail.spaces += spaces;
     }
     
-    public void breakWheel(){
-        this.wagonFunctional = false;
+    public void breakWheel(Wheel wheel){
+        if(wheel.getCount() == 0){
+            this.wagonFunctional = false;
+        } else {
+            wheel.setCount(wheel.getCount()-1);
+        }
     }
 
-    public void breakAxle(){
-
+    public void breakAxle(Axle axle){
+        if(axle.getCount() == 0){
+            this.wagonFunctional = false;
+        } else {
+            axle.setCount(axle.getCount()-1);
+        }
     }
 
-    public void breakTongue(){
-        
+    public void breakTongue(Tongue tongue){
+        if(tongue.getCount() == 0){
+            this.wagonFunctional = false;
+        } else {
+            tongue.setCount(tongue.getCount()-1);
+        }
     }
 
-    public void oxDies(){
+    public void oxDies(Oxen ox){
+        if(ox.getCount() == 0){
+            this.wagonFunctional = false;
+        } else {
+            ox.setCount(ox.getCount()-1);
+        }
+    }
 
+    public void memberDies(WagonMember member){
+        this.livingMembers--;
+        this.listOfMembers.remove(member);
     }
 
     public void updateMoney(double amount){
         this.moneyLeft -= amount;
     }
 
-    public void huntForFood(int c){
-
+    public void huntForFood(Bullets bullets, Food food){
+        Random rand = new Random();
+        int random = rand.nextInt(5);
+        if(random == 1){ // 1/5 chance of shooting something
+            random = rand.nextInt(8); // different probabilities of type of animal hunted
+            if(random == 0 || random == 1 || random == 2){
+                System.out.println("You shot a rabbit. +4 lbs of food.");
+                food.setCount(food.getCount()+4);
+            } else if(random == 3 || random == 4 || random == 5){
+                System.out.println("You shot a squirrel. +4 lbs of food.");
+                food.setCount(food.getCount()+4);
+            } else if(random == 6 || random == 7){
+                System.out.println("You shot a deer. +50 lbs of food.");
+                food.setCount(food.getCount()+50);
+            } else {
+                System.out.println("You shot a bear. +100 lbs of food.");
+                food.setCount(food.getCount()+100);
+            }
+        }
+        bullets.setCount(bullets.getCount()-1);
     }
 }
