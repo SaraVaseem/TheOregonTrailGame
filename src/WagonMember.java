ca3e4f53dbf6fb6940acbcc8bc87cabee05bc37e
Sara Vaseem <saranausheen18@gmail.com>
2024-04-13 14:01:56 -0700

    added functions and test scripts to WagonMember class

--- src/WagonMember.java
+++ src/WagonMember.java
@@ -14,30 +14,63 @@ public class WagonMember {
     boolean starving;
 
     public static void die() {
-
+        if(health==0) {
+            alive = false;
+        }
+        System.out.println(name + " has died.");
     }
 
     public static void updateHealth(String pace, String rations) {
+        if(pace.equals("Steady")) {
+            health += 2;
+        } else if(pace.equals("Strenuous")) {
+            health -= 1;
+        } else if(pace.equals("Grueling")) {
+            health -= 4;
+        }
 
+        if(rations.equals("Filling")) {
+            health += 2;
+        } else if(rations.equals("Meager")) {
+            health -= 1;
+        } else if(rations.equals("Bare Bones")) {
+            health -= 2;
+        }
     }
 
     public static void updateDisease() {
-
+        if(diseaseRisk > 0.5) {
+            disease = Disease.getRandomDisease();
+        }
     }
 
     public static void updateDiseaseRisk() {
-
+        if(immunity) {
+            diseaseRisk = 0;
+        } else {
+            diseaseRisk = 0.5;
+        }
     }
 
     public static void updateImmunity() {
-
+        if(disease != null) {
+            immunity = disease.isContagious();
+        }
     }
 
     public static void updateImmunityDuration() {
-
+        if(immunity) {
+            immunityDuration = 5;
+        } else {
+            immunityDuration = 0;
+        }
     }
 
     public static void updateStarving() {
-
+        if(rations.equals("Bare Bones")) {
+            starving = true;
+        } else {
+            starving = false;
+        }
     }    
 }
